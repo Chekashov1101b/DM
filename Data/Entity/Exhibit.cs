b29e845608030ef1010797e0dfc51c283ed4375f
Chekashov1101b <chma230202@mail.ru>
2022-05-05 02:42:59 +0500

    1.01

--- Data/Entity/Exhibit.cs
+++ Data/Entity/Exhibit.cs
@@ -10,17 +10,22 @@ namespace DM.Data.Entity
     {
         public int Id { get; set; }
         [Display(Name = "Название экспоната")]
+        [StringLength(45, MinimumLength = 3)]
         public string Name { get; set; }
         [Display(Name = "Век")]
+        [Range(1, Int32.MaxValue)]
         public int Century { get; set; }
         [Display(Name = "Нашей эры?")]
         public bool Era { get; set; }
         [Display(Name = "Место хранения")]
+        [StringLength(45, MinimumLength = 3)]
         public string Place { get; set; }
         [Display(Name = "Стоимость")]
         [DataType(DataType.Currency)]
+        [Range(1, Int32.MaxValue)]
         public int Cost { get; set; }
         [Display(Name = "Краткое описание")]
+        [StringLength(120, MinimumLength = 10)]
         public string Discription { get; set; }
         [Display(Name = "Изображение")]
         public byte[] Image { get; set; }
